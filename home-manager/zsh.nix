2168c1cb671b12a091f50d8d657707fd6f3a6e67
Jeremy Hettenhouser <jhettenh@gmail.com>
2025-07-20 20:28:20 -0400

    Added zsh alias corrections

--- home-manager/zsh.nix
+++ home-manager/zsh.nix
@@ -12,17 +12,17 @@
 
       rsync-bak = "rsync -avbuzh --numeric-ids --compress-choice=zstd --progress --backup-dir={{.xdg_cache_home}}/rsync";
 
-      alias cat="bat --pager=never";
-      alias top="sudo htop";
-      alias du="ncdu --color dark -rr -x --exclude .git --exclude node_modules";
+      cat="bat --pager=never";
+      top="sudo htop";
+      du="ncdu --color dark -rr -x --exclude .git --exclude node_modules";
 
-      alias ls="eza --icons";                                   # ls
-      alias l="eza -lbF --git --icons";                         # list, size, type, git
-      alias ll="eza -lbGF --git --icons";                       # long list
-      alias ltr="eza -lbGd --git --sort=modified --icons";      # long list, modified date sort
-      alias llm="eza --all --header --long --sort=modified $eza_params";
-      alias la="eza -lbhHigUmuSa --git --color-scale --icons";  # all list
-      alias lx="eza -lbhHigUmuSa@ --git --color-scale --icons"; # all + extended list
+      ls="eza --icons";                                   # ls
+      l="eza -lbF --git --icons";                         # list, size, type, git
+      ll="eza -lbGF --git --icons";                       # long list
+      ltr="eza -lbGd --git --sort=modified --icons";      # long list, modified date sort
+      llm="eza --all --header --long --sort=modified $eza_params";
+      la="eza -lbhHigUmuSa --git --color-scale --icons";  # all list
+      lx="eza -lbhHigUmuSa@ --git --color-scale --icons"; # all + extended list
       update = "sudo nixos-rebuild switch";
     };
     history.size = 10000;
