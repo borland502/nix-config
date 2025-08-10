effa796918cfd9bc0f7adf2219b33fe996810702
Jeremy Hettenhouser <jhettenh@gmail.com>
2025-08-10 15:42:55 -0400

    Remove allowance for unfree packages in Nix configuration and just use global

--- hosts/krile/default.nix
+++ hosts/krile/default.nix
@@ -117,9 +117,6 @@
 
   security.sudo.wheelNeedsPassword = false;
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
