d58dd29e8b42c6a49e9c36fc95e0aea6cd587612
Jeremy Hettenhouser <jeremy@example.com>
2025-08-13 20:52:00 -0400

    Use Flameshot for screenshots: exclude Spectacle, add Plasma shortcut, configure Flameshot service

--- modules/desktop/plasma.nix
+++ modules/desktop/plasma.nix
@@ -16,6 +16,9 @@
   
   services.desktopManager.plasma6.enable = true;
 
+  # Remove Spectacle from the default Plasma apps bundle
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [ spectacle ];
+
   # Enable touchpad support (enabled by default in most desktop managers)
   # services.xserver.libinput.enable = true;
 }
