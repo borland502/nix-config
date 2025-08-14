d5816c4fb86d9e5b15772dfa0811cba514027cbc
Jeremy Hettenhouser <jeremy.hettenhouser@icf.com>
2025-08-14 13:47:14 -0400

    Add Linux-specific configuration for home-manager

---
+++ home-manager/home-linux.nix
@@ -0,0 +1,64 @@
+{ config, pkgs, lib, ... }:
+
+{
+  imports = [
+    ./common.nix           # Import common configuration
+    ./profiles/development.nix
+    ./profiles/desktop.nix
+  ];
+
+  home.username = "jhettenh";
+  home.homeDirectory = lib.mkForce "/home/jhettenh";
+
+  # Linux-specific packages
+  home.packages = with pkgs; [
+    # Linux-specific system monitoring
+    iotop
+    iftop
+    strace
+    ltrace
+    sysstat
+    lm_sensors
+    ethtool
+    pciutils
+    usbutils
+  ];
+
+  # Linux-specific Stylix targets (extending common.nix)
+  stylix.targets = {
+    kitty.enable = true;
+    gtk.enable = true;
+    kde.enable = true;
+    qt.enable = false;
+    firefox.enable = true;
+  };
+
+  # Linux-specific font fallbacks
+  fonts.fontconfig.defaultFonts = {
+    sansSerif = lib.mkAfter [ "DejaVu Sans" ];
+    serif = lib.mkAfter [ "DejaVu Serif" ];
+  };
+
+  # Linux-specific services
+  services.flameshot = {
+    enable = true;
+    settings = {
+      General = {
+        disabledTrayIcon = false;
+        showStartupLaunchMessage = false;
+        savePath = "/home/jhettenh/Pictures/Screenshots";
+        savePathFixed = true;
+      };
+    };
+  };
+
+  # Linux-specific KDE shortcuts
+  programs.plasma = {
+    enable = true;
+    shortcuts = {
+      "flameshot" = {
+        "Capture" = "Ctrl+Shift+S";
+      };
+    };
+  };
+}
